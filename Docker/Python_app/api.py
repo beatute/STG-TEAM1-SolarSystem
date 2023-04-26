7cca3888d2c6b15a3c2d80d19176fb74ad519ce7
beatute <110552465+beatute@users.noreply.github.com>
2023-04-26 15:15:57 +0300

    Update api.py

--- Docker/Python_app/api.py
+++ Docker/Python_app/api.py
@@ -12,7 +12,7 @@ def create_planets(planets):
     return planets, status_code
 
 
-def read_one_planets(id):
+def read_one_planets(planet_id):
     return db_data.get_one_planet(planet_id), 200
 
 
