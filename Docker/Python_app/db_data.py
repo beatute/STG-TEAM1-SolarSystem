599a37683b377efe660c157f7d7a5f6d23996a83
beatute <110552465+beatute@users.noreply.github.com>
2023-04-26 22:11:47 +0300

    Update db_data.py

--- Docker/Python_app/db_data.py
+++ Docker/Python_app/db_data.py
@@ -1,70 +1,68 @@
 import db_connect as db
 
-def get_all_planets():
-    planets_list = []
+def get_all_planet():
+    planet_list = []
     with db.create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT id, name, orbital_period_days, average_speed_kms, satellites, mean_radius, temperature_c FROM planets")
-            for planets in cursor.fetchall():
-                planets_dict = {
-                    'id': planets[0],
-                    'name': planets[1],
-                    'orbital_period_days': planets[2],
-                    'average_speed_kms': planets[3],
-                    'satellites': planets[4],
-                    'mean_radius': planets[5],
-                    'temperature_c': planets[6]
+            cursor.execute("SELECT name, orbital_period_days, average_speed_kms, satellites, mean_radius, temperature_c FROM planets")
+            for planet in cursor.fetchall():
+                planet_dict = {
+                    'name': planet[0],
+                    'orbital_period_days': planet[1],
+                    'average_speed_kms': planet[2],
+                    'satellites': planet[3],
+                    'mean_radius': planet[4],
+                    'temperature_c': planet[5]
                 }
-                planets_list.append(planets_dict)
-    return planets_list
+                planet_list.append(planet_dict)
+    return planet_list
 
-def create_new_planets(planets):
-    existing_planets = get_one_planet(planets['id'])
-    if existing_planets:
-        return {'error': f" Planets with id {planets['id']} already exists"}, 409
+def create_new_planet(planet):
+    existing_planet = get_one_planet(planet['name'])
+    if existing_planet:
+        return {'error': f" Planets with name {planet['name']} already exists"}, 409
 
     with db.create_connection() as connection:
         with connection.cursor() as cursor:
-            query = "INSERT INTO planets (id, name, orbital_period_days, average_speed_kms, satellites, mean_radius, temperature_c) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-            values = (planets['id'], planets['name'], planets['orbital_period_days'],  planets['average_speed_kms'], planets['satellites'], planets['mean_radius'], planets['temperature_c'])
+            query = "INSERT INTO planets (name, orbital_period_days, average_speed_kms, satellites, mean_radius, temperature_c) VALUES (%s, %s, %s, %s, %s, %s);"
+            values = (planet['name'], planet['orbital_period_days'], planet['average_speed_kms'], planet['satellites'], planet['mean_radius'], planet['temperature_c'])
             cursor.execute(query, values)
             connection.commit()
-    return planets, 201
+    return planet, 201
 
 
-def get_one_planet(id):
+def get_one_planet(name):
     with db.create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT id, name, orbital_period_days, average_speed_kms, satellites, mean_radius, temperature_c FROM planets WHERE id ='" + id + "'")
-            planets = cursor.fetchone()
-            if planets is not None:
+            cursor.execute("SELECT name, orbital_period_days, average_speed_kms, satellites, mean_radius, temperature_c FROM planets WHERE name ='" + name + "'")
+            planet = cursor.fetchone()
+            if planet is not None:
                 return {
-                    'id': planets[0],
-                    'name': planets[1],
-                    'orbital_period_days': planets[2],
-                    'average_speed_kms': planets[3],
-                    'satellites': planets[4],
-                    'mean_radius': planets[5],
-                    'temperature_c': planets[6]
+                    'name': planet[0],
+                    'orbital_period_days': planet[1],
+                    'average_speed_kms': planet[2],
+                    'satellites': planet[3],
+                    'mean_radius': planet[4],
+                    'temperature_c': planet[5]
                 }
             else:
                 return None
 
 
-def delete_planets(id):
+def delete_planet(name):
     with db.create_connection() as conn:
         with conn.cursor() as cursor:
-            delete_query = "DELETE FROM planets WHERE id ='" + id + "'"
+            delete_query = "DELETE FROM planets WHERE name ='" + name + "'"
             cursor.execute(delete_query)
             conn.commit()
             return cursor.rowcount
 
 
-def update_planets(id, planets):
+def update_planet(name, planet):
     with db.create_connection() as conn:
         with conn.cursor() as cursor:
-            update_query = "UPDATE planets SET id = %s, name = %s, orbital_period_days = %s, average_speed_kms = %s, satellites = %s, mean_radius = %s, temperature_c = %s WHERE id = %s"
-            values = (planets['name'], planets['orbital_period_days'], planets['average_speed_kms'], planets['satellites'], planets['mean_radius'], planets['temperature_c'], id)
+            update_query = "UPDATE planets SET orbital_period_days = %s, average_speed_kms = %s, satellites = %s, mean_radius = %s, temperature_c = %s WHERE name = %s"
+            values = (planet['orbital_period_days'], planet['average_speed_kms'], planet['satellites'], planet['mean_radius'], planet['temperature_c'], name)
             cursor.execute(update_query, values)
             conn.commit()
-        return planets
+        return planet
